a398c799d75869abdc6660fedf474dae5240f3e7
Renato <ti.rp301223@gmail.com>
2025-06-04 00:52:53 -0300

    novo

--- firebase.js
+++ firebase.js
@@ -1,29 +1,18 @@
-// firebase.js atualizado
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import {
-  getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot
-} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyIEHrdcMttMA1RylqqG_wokheP5EcIc4",
   authDomain: "presentescasal.firebaseapp.com",
   projectId: "presentescasal",
-  storageBucket: "presentescasal.appspot.com",
+  storageBucket: "presentescasal.firebasestorage.app",
   messagingSenderId: "518416733032",
-  appId: "1:518416733032:web:c6ccf8b438b5563864bcdd",
-  measurementId: "G-VECFNQVX87"
+  appId: "1:518416733032:web:227913c1085f350f64bcdd",
+  measurementId: "G-ZWTZTC1GYM"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export {
-  db,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc,
-  onSnapshot
-};
+export { db };
